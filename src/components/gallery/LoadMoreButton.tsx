e52b3b96fdf5ff8825a265742f3677257a3ee06f
sdfiz <sdfizzz@gmail.com>
2021-07-09 08:35:49 +0400

    fix unsupported css properties flex gap safari from 14.1

--- src/components/gallery/LoadMoreButton.tsx
+++ src/components/gallery/LoadMoreButton.tsx
@@ -18,7 +18,7 @@ const LoadMoreButton = () => {
         <Spinner />
       ) : (
         <>
-          See more
+          <span>See more</span>
           <svg width="7" height="17" viewBox="0 0 7 17" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M1 1L5.043 8.41667L1 15.8333" stroke="#363636" strokeWidth="2" strokeLinecap="round" />
           </svg>
