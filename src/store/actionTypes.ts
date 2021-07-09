24ae96973f889a2a6f019ac9ba65bc1de78d1b01
sdfiz <sdfizzz@gmail.com>
2021-07-09 11:50:24 +0400

    fix action type definitions

--- src/store/actionTypes.ts
+++ src/store/actionTypes.ts
@@ -1,6 +1,8 @@
-export const ADD_CARDS = 'ADD_CARDS';
+export type ADD_CARDS_Type = 'ADD_CARDS';
+export const ADD_CARDS: ADD_CARDS_Type = 'ADD_CARDS';
 
 export const START_LOADING = 'START_LOADING';
 export const END_LOADING = 'END_LOADING';
 
-export const FILTER_CARDS = 'FILTER_CARDS';
+export type FILTER_CARDS_Type = 'FILTER_CARDS';
+export const FILTER_CARDS: FILTER_CARDS_Type = 'FILTER_CARDS';
--- src/store/types.ts
+++ src/store/types.ts
@@ -1,4 +1,4 @@
-import { ADD_CARDS, FILTER_CARDS } from './actionTypes';
+import { ADD_CARDS_Type, FILTER_CARDS_Type } from './actionTypes';
 
 export type CardType = 'IndependentLiving' | 'SupportAvailable';
 
@@ -21,12 +21,12 @@ export type Action = {
 };
 
 export type AddCardAction = Action & {
-  type: ADD_CARDS;
+  type: ADD_CARDS_Type;
   cards: CardPropType[];
 };
 
 export type FilterAction = Action & {
-  type: FILTER_CARDS;
+  type: FILTER_CARDS_Type;
   filter: string;
 };
 
